ff22df9f0384eba3ab05fed4b9879557657b9315
Anthony Lee <anthonylee@umass.edu>
2019-04-12 17:40:34 -0400

    Added image files and autolayout for buttons

--- HackingWithSwift_Project_2/ViewController.swift
+++ HackingWithSwift_Project_2/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonOne: UIButton!
+    @IBOutlet var buttonTwo: UIButton!
+    @IBOutlet var buttonThree: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+        buttonOne.layer.borderColor = UIColor.lightGray.cgColor
+        buttonTwo.layer.borderColor = UIColor.lightGray.cgColor
+        buttonThree.layer.borderColor = UIColor.lightGray.cgColor
+        
+        buttonOne.layer.borderWidth = 1
+        buttonTwo.layer.borderWidth = 1
+        buttonThree.layer.borderWidth = 1
+    }
+    
+    func askQuestion(){
+        buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
+        buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
+        buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
+        
     }
 
 
