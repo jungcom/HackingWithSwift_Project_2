24e8a33fc995ff2c1ee093ad36ff437c5e1cae38
Anthony Lee <anthonylee@umass.edu>
2019-04-15 17:25:28 -0400

    Finished Project 2

--- HackingWithSwift_Project_2/ViewController.swift
+++ HackingWithSwift_Project_2/ViewController.swift
@@ -16,10 +16,12 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         countries.append("estonia")
         countries.append("france")
         countries.append("germany")
@@ -40,15 +42,39 @@ class ViewController: UIViewController {
         buttonOne.layer.borderWidth = 1
         buttonTwo.layer.borderWidth = 1
         buttonThree.layer.borderWidth = 1
+        
+        askQuestion()
     }
     
     func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        navigationItem.title = countries[correctAnswer].uppercased()
+        
         buttonOne.setImage(UIImage(named: countries[0]), for: .normal)
         buttonTwo.setImage(UIImage(named: countries[1]), for: .normal)
         buttonThree.setImage(UIImage(named: countries[2]), for: .normal)
         
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Incorrect"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Continue", style: .default) { (_) in
+            self.askQuestion()
+        }
+        ac.addAction(alertAction)
+        present(ac, animated: true, completion: nil)
+    }
+    
+    
 }
 
